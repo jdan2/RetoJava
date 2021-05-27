cf22a6dad4926021e2621e304830ba57eba85374
daniel <juandortiz.15@gmail.com>
2021-05-27 16:18:32 -0500

    Se realizo el punto 1

---
+++ src/main/java/co/com/sofka/reto/punto1/Punto1.java
@@ -0,0 +1,20 @@
+package co.com.sofka.reto.punto1;
+
+import java.util.Scanner;
+
+public class Punto1 {
+    public static void main(String[] args) {
+
+        int num1 = 7;
+        int num2 = 7;
+
+        if(num1 > num2){
+            System.out.println("Numero uno es mayor");
+        }else if (num1 < num2){
+            System.out.println("Numero dos es mayor");
+        }else{
+            System.out.println("Los numeros son iguales");
+        }
+
+    }
+}

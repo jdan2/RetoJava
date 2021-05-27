816a377bb6f3e9249f349f0f7d03904dd7802ce3
daniel <juandortiz.15@gmail.com>
2021-05-27 18:25:41 -0500

    Se realizo el punto 9

---
+++ src/main/java/co/com/sofka/reto/punto9/Punto9.java
@@ -0,0 +1,11 @@
+package co.com.sofka.reto.punto9;
+
+public class Punto9 {
+    public static void main(String[] args) {
+
+        String frase1 = "La sonrisa sera la mejor arma contra la tristeza";
+        frase1 = frase1.replace('a','e');
+
+        System.out.println(frase1 + " siempre de los siempre" );
+    }
+}

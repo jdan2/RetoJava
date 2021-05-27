3c2afc784a4e91f4e55afebe828e50bea73f507f
daniel <juandortiz.15@gmail.com>
2021-05-27 16:55:21 -0500

    Se realizo el punto 6

---
+++ src/main/java/co/com/sofka/reto/punto6/Punto6.java
@@ -0,0 +1,16 @@
+package co.com.sofka.reto.punto6;
+
+public class Punto6 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i<101 ; i++){
+
+            if( i%2 == 0){
+                System.out.println(i + " Es par");
+            }else {
+                System.out.println(i + " Es impar");
+            }
+
+        }
+    }
+}
